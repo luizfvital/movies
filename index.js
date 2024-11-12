e40efb50f7e87f22fcaa28d687ae3be03abaa7ec
Luiz Fernando Vital <luizfvital@gmail.com>
2024-11-11 21:26:23 -0300

    feat: add axios

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'aea35f02',
+      s: 'avengers',
+    }
+  });
+
+  console.log(response.data);
+}
+
+fetchData();
