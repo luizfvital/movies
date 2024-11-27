122ca66ce03e4052c16abaf38231e728632f0d1b
Luiz Fernando Vital <luizfvital@gmail.com>
2024-11-26 21:17:35 -0300

    feat: make autocomplete reusable

--- index.js
+++ index.js
@@ -20,11 +20,19 @@ const fetchData = async (searchTerm) => {
   );
 }
 
-const onMovieSelected = async (id) => {
+const renderMovieOption = (movie) => {
+  const imgSrc = movie.poster === 'N/A' ? '' : movie.poster;
+  return `
+    <img src="${imgSrc}" alt="${movie.title}"/>
+    ${movie.title}
+  `;
+}
+
+const onMovieSelected = async (movie) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: 'aea35f02',
-      i: id,
+      i: movie.imdbID,
     }
   });
 
@@ -80,58 +88,10 @@ const movieTemplate = (movieDetail) => {
   `;
 }
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search for a movie</b></label>
-  <input class="input" type="search"/>
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (e) => {
-  const movies = await fetchData(e.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-
-  resultsWrapper.innerHTML = '';
-
-  dropdown.classList.add('is-active');
-
-  for (let movie of movies) {
-    const option = document.createElement('a');
-
-    const imgSrc = movie.poster === 'N/A' ? '' : movie.poster;
- 
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-      <img src="${imgSrc}" alt="${movie.title}"/>
-      ${movie.title}
-    `;
-
-    option.addEventListener('click', e => {
-      dropdown.classList.remove('is-active');
-      input.value = movie.title;
-
-      onMovieSelected(movie.imdbId);
-    });
-
-    resultsWrapper.appendChild(option);
-  }
-}
-
-input.addEventListener('input', debounce(onInput));
-document.addEventListener('click', e => {
-  if (!root.contains(e.target)) {
-    dropdown.classList.remove('is-active');
-  }
+createAutocomplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption: () => renderMovieOption(movie),
+  onOptionSelected: () => onMovieSelected(movie),
+  inputValue: (movie) => movie.title,
+  fetchData: async () => fetchData(searchTerm),
 });
