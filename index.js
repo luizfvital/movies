000ae5940c3ed38cce69ddc9b1f9572409da8226
Luiz Fernando Vital <luizfvital@gmail.com>
2024-12-02 21:44:16 -0300

    feat: parse data for comparison

--- index.js
+++ index.js
@@ -1,80 +1,57 @@
-const fetchData = async (searchTerm) => {
+let leftMovie, rightMovie;
+
+const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: 'aea35f02',
-      s: searchTerm,
+      apikey: 'd9835cc5',
+      i: movie.imdbID
     }
   });
 
-  if (response.data.Error) {
-    return [];
+  summaryElement.innerHTML = movieTemplate(response.data);
+
+  if (side === 'left') {
+    leftMovie = response.data;
+  } else {
+    rightMov
+    
   }
 
-  return response.data.Search.map(movie => ({
-      poster: movie.Poster,
-      title: movie.Title,
-      type: movie.Type,
-      year: movie.Year,
-      imdbId: movie.imdbID
-    })
-  );
+  if(leftMovie && rightMovie) {
+    runComparison();
+  }
+};
+
+const runComparison = () => {
+  console.log('run comparison');
 }
 
-const renderMovieOption = (movie) => {
-  const imgSrc = movie.poster === 'N/A' ? '' : movie.poster;
-  return `
-    <img src="${imgSrc}" alt="${movie.title}"/>
-    ${movie.title}
-  `;
-}
-
-const onMovieSelected = async (movie) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: 'aea35f02',
-      i: movie.imdbId,
-    }
-  });
-
-  summary.innerHTML = movieTemplate({
-    poster: response.data.Poster,
-    title: response.data.Title,
-    genre: response.data.Genre,
-    plot: response.data.Plot,
-    awards: response.data.Awards,
-    boxOffice: response.data.BoxOffice,
-    metascore: response.data.Metascore,
-    imdbRating: response.data.imdbRating,
-    imdbVotes: response.data.imdbVotes
-  });
-}
-
-const movieTemplate = (movieDetail) => {
+const movieTemplate = movieDetail => {
   return `
     <article class="media">
       <figure class="media-left">
         <p class="image">
-          <img src="${movieDetail.poster}" />
+          <img src="${movieDetail.Poster}" />
         </p>
       </figure>
       <div class="media-content">
         <div class="content">
-          <h2>${movieDetail.title}</h2>
-          <h4>${movieDetail.genre}</h4>
-          <p>${movieDetail.plot}</p>
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
         </div>
       </div>
     </article>
     <article class="notification is-primary">
-      <p class="title">${movieDetail.awards}</p>
+      <p class="title">${movieDetail.Awards}</p>
       <p class="subtitle">Awards</p>
     </article>
     <article class="notification is-primary">
-      <p class="title">${movieDetail.boxOffice}</p>
+      <p class="title">${movieDetail.BoxOffice}</p>
       <p class="subtitle">Box Office</p>
     </article>
     <article class="notification is-primary">
-      <p class="title">${movieDetail.metascore}</p>
+      <p class="title">${movieDetail.Metascore}</p>
       <p class="subtitle">Metascore</p>
     </article>
     <article class="notification is-primary">
@@ -86,12 +63,49 @@ const movieTemplate = (movieDetail) => {
       <p class="subtitle">IMDB Votes</p>
     </article>
   `;
-}
+};
+
+const autocompleteConfig = {
+  renderOption(movie) {
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+    return `
+      <img src="${imgSrc}" />
+      ${movie.Title} (${movie.Year})
+    `;
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
+  }
+};
 
 createAutocomplete({
-  root: document.querySelector('.autocomplete'),
-  renderOption: (movie) => renderMovieOption(movie),
-  onOptionSelected: (movie) => onMovieSelected(movie),
-  inputValue: (movie) => movie.title,
-  fetchData: (searchTerm) => fetchData(searchTerm),
+  ...autocompleteConfig,
+  onOptionSelected(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
+  },
+  root: document.querySelector('#left-autocomplete')
+});
+
+createAutocomplete({
+  ...autocompleteConfig,
+  onOptionSelected(movie) {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
+  },
+  root: document.querySelector('#right-autocomplete')
 });
